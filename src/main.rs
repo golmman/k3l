1b15b175d21fe610958beee07d6b2b0a88608c0f
Dirk Kretschmann <kretschmanndi@gmail.com>
2022-01-31 22:57:27 +0100

    added screen implementation

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,54 @@ use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
+pub struct Screen<W: Write> {
+    out: W,
+    terminal_cols: i32,
+    terminal_rows: i32,
+}
+
+impl<W: Write> Screen<W> {
+    pub fn new(mut out: W) -> Self {
+        write!(
+            out,
+            "{}{}{}",
+            termion::cursor::Hide,
+            termion::cursor::Goto(1, 1),
+            termion::clear::All,
+        )
+        .unwrap();
+
+        Self {
+            out,
+            terminal_cols: 0,
+            terminal_rows: 0,
+        }
+    }
+}
+
+impl<W: Write> Drop for Screen<W> {
+    fn drop(&mut self) {
+        write!(
+            self,
+            "{}{}{}",
+            termion::clear::All,
+            termion::cursor::Goto(1, 1),
+            termion::cursor::Show,
+        )
+        .unwrap();
+    }
+}
+
+impl<W: Write> Write for Screen<W> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.out.write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.out.flush()
+    }
+}
+
 pub enum Event {
     Key(Key),
 }
@@ -14,19 +62,31 @@ pub enum Event {
 pub fn send_key_events(sender: SyncSender<Event>) {
     let stdin = stdin();
 
-    for key in stdin.keys() {
-        if let Ok(key) = key {
-            let _ = sender.send(Event::Key(key));
-        }
+    for key in stdin.keys().flatten() {
+        let _ = sender.send(Event::Key(key));
     }
 }
 
-pub fn receive_event(receiver: &Receiver<Event>, screen: &mut dyn Write) -> bool {
+pub fn receive_event<W: Write>(receiver: &Receiver<Event>, screen: &mut Screen<W>) -> bool {
     let event = receiver.recv().unwrap();
 
-    write!(screen, "{}", termion::clear::All);
+    write!(
+        screen,
+        "{}{}",
+        termion::cursor::Goto(10, 3),
+        termion::clear::All,
+    )
+    .unwrap();
+
     match event {
-        Event::Key(key) => write!(screen, "{key:?}\n").unwrap(),
+        Event::Key(key) => {
+            writeln!(screen, "{key:?}").unwrap();
+
+            match key {
+                termion::event::Key::Char('q') => return false,
+                _ => {}
+            }
+        }
     }
 
     screen.flush().unwrap();
@@ -34,25 +94,10 @@ pub fn receive_event(receiver: &Receiver<Event>, screen: &mut dyn Write) -> bool
     true
 }
 
-fn drop(screen: &mut dyn Write) {
-    write!(
-        screen,
-        "{}{}{}",
-        termion::clear::All,
-        termion::cursor::Goto(1, 1),
-        termion::cursor::Show,
-    )
-    .unwrap();
-}
-
 fn main() {
     println!("Hello, world!");
 
-    let mut screen = stdout().into_raw_mode().unwrap();
-
-    //drop(&mut screen);
-    //return;
-
+    let mut screen = Screen::new(stdout().into_raw_mode().unwrap());
 
     write!(
         screen,
